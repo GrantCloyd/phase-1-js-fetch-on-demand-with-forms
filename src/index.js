a32186556e2535084a15d6c7d1bde38881bdd97f
Grant Cloyd <dgcloyd@gmail.com>
2021-05-24 20:02:39 -0500

    commit tree 18facb345d344dcbf12b596e2806e78adda2a208

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+  formFunc();
+};
+
+function formFunc() {
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = e.target.search.value;
+    fetch(`http://localhost:3000/movies/${input}`)
+      .then((response) => response.json())
+      .then((json) => {
+        movieDetails = document.querySelector("#movieDetails");
+        const title = movieDetails.querySelector("h4");
+        const summary = movieDetails.querySelector("p");
+        title.textContent = json.title;
+        summary.textContent = json.summary;
+        console.log(json);
+      });
+  });
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
